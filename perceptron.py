6530fb5374becfbdc409d4523df4d55af3bf9968
Andrew <arusk2@pdx.edu>
2021-04-24 12:57:56 -0700

    Draft 1 of training NN. Debugging forward prop and back prop next

--- perceptron.py
+++ perceptron.py
@@ -22,14 +22,17 @@ class Perceptron():
         # setting these to 0 for backprop later delta at time t - 1 for weights
         self.delta_tm1_Wkj = np.zeros((self.nhidden+1, 10))
         self.delta_tm1_Wij = np.zeros((len(inputs[0], self.nhidden)))
-        pass
+
+        print("training 1")
+        self.forwardProp(inputs[0])
+        self.checkCorrect(inputs[0],labels[0])
 
     def forwardProp(self, input):
         # get activation of hidden layer, excluding bias node.
         self.hidden_units[:self.nhidden] = np.dot(input, self.weights1)
         self.output = np.dot(self.hidden_units, self.weights2)
 
-    def checkCorrect(self, label):
+    def checkCorrect(self, input, label):
         # If correct, return, move onto next input. If wrong, do backprop
         prediction = np.argmax(self.output)
         # Set max prediction to .9
@@ -38,11 +41,11 @@ class Perceptron():
         self.output = np.where(self.output == .9, .9, .1)
         label = np.where(label == 1., .9, .1)
         if prediction != np.argmax(label):
-            # back prop
+            self.backProp(input, label)
         # Return if correct
         return
 
-    def backProp(self, label):
+    def backProp(self, input, label):
         # We got the prediction wrong, need to backprop
         # First, set output for sigmoid fn ( .9 already set for max prediction in checkCorrect() )
         self.output = np.where(self.output == .9, .9, .1)
@@ -51,14 +54,23 @@ class Perceptron():
         #   Delta(Wkj) = eta * sigk * Hj + momentum * (deltaT-1(Wkj))
         # Calculate error term sigk for output units, will add Hj as weights are updating.
         sigmak = self.output(1 - self.output)(label-self.output) # should be 1x10
-        # array of weight kj delta, where k is output array. This array is how all weights should change
-        # for each row j in weight array. Just need to multiply by value of each hidden unit corresponding
-        # to all the weights we're updating.
-        delta_Wkj = self.eta * sigmak
-        for j in range(len(self.weights2)): #updating row j with 1x10 of k Wkj
-            delta_Wkj += self.hidden_units[j]  # eta * sigk * Hj
-            delta_Wkj += (self.momentum * self.delta_tm1_Wkj[j])  # + (momentum * deltaWt-1(kj)
-            self.weights2[j] = self.weights2[j] - delta_Wkj
-            self.delta_tm1_Wkj[j] = delta_Wkj  # Updating for next time step
+        # Np.outer gives 10xn+1 matrix of Each Sigma multiplied by each Hj, resulting in a matrix
+        # of sigmak * hj for all combinations of sigmak and hj such that row 1 is W1*H1 ... W1Hn
+        # self.delta_tm1_wkj is a n+1x10 matrix, so I had to transpose the np.outer one to do elementwise add
+        delta_Wkj = self.eta * np.transpose(np.outer(sigmak, self.hidden_units)) + (self.momentum*self.delta_tm1_Wkj)
+        # ^ this should currently be n+1x10
+        self.weights2 = self.weights2 - delta_Wkj
+        #  ^ should be n+1x10
+        self.delta_tm1_Wkj = delta_Wkj
+        # ^ should be n+1x10
+
+        # Now Updating weights from input to hidden (Wij)
         # when updating input to hidden weights, make sure to stop at hidden[:nhidden] to avoid bias
-        pass
+        # self.hidden = 1x20 vector; self.weights2 = n+1x10, np.transpose(sigmak) = 10x1
+        # sigmaj should be 1x20 vector, each sum of all weights to hidden unit j
+        sigmaj = self.hidden_units (1-self.hidden_units) * np.dot(self.weights2, np.transpose(sigmak))
+        #                               V sigmaj now 20x1   V 1x785
+        delta_Wij = self.eta * np.transpose(np.dot(np.transpose(sigmaj)), input) + (self.momentum * self.delta_tm1_Wij)
+        #                       ^ Makes this array 785xn+1, just like delta_tm1_Wij
+        self.weights1 = self.weights1 - delta_Wij
+        return
