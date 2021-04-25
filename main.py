7622003737f6e25278095e7eafb787fc441d2c01
Andrew <arusk2@pdx.edu>
2021-04-25 07:43:34 -0700

    Draft 2. Debugging performance issues

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # experiment goes here. Another file has all the class stuff for perceptron
 import keras
+
 import numpy as np
 from keras.datasets import mnist
+import perceptron
 import matplotlib.pyplot as plt
 
 #creating a perceptron network to classify MNIST data
@@ -23,3 +25,75 @@ xtrain = np.hstack((xtrain, np.atleast_2d(ones).T))
 # ytrain =
 ones = np.ones(10000)
 xtest = np.hstack((xtest, np.atleast_2d(ones).T))
+
+# Experiment 1
+nhidden = [20, 50, 100]
+eta = .1
+momentum = .9
+epochs = 50
+for n in nhidden:
+    test = perceptron.Perceptron(epochs, n, eta, momentum)
+    # train() shuffles inputs and so the order is not always the same
+    conf_matrix, tr_acc, test_acc = test.train(xtrain, ytrain, xtest, ytest)
+    tr_acc = np.array(tr_acc)
+    tr_acc *= 100
+    test_acc = np.array(test_acc)
+    test_acc *= 100
+    print(conf_matrix)
+    plt.plot(tr_acc, label="Training Accuracy")
+    plt.plot(test_acc, label="Testing Accuracy")
+    plt.xlabel('Epochs')
+    plt.ylabel('Percent Correct')
+    plt.title(f"Testing vs Training Accuracy with {n} Hidden Units")
+    plt.legend()
+    plt.show()
+    plt.clf()
+
+# Experiment 2
+nhidden = 100
+eta = .1
+momentum = [0, .25, .5]
+epochs = 50
+for m in momentum:
+    test = perceptron.Perceptron(epochs, nhidden, eta, m)
+    # train() shuffles inputs and so the order is not always the same
+    conf_matrix, tr_acc, test_acc = test.train(xtrain, ytrain, xtest, ytest)
+    tr_acc = np.array(tr_acc)
+    tr_acc *= 100
+    test_acc = np.array(test_acc)
+    test_acc *= 100
+    print(conf_matrix)
+    plt.plot(tr_acc, label="Training Accuracy")
+    plt.plot(test_acc, label="Testing Accuracy")
+    plt.xlabel('Epochs')
+    plt.ylabel('Percent Correct')
+    plt.title(f"Testing vs Training Accuracy with Momentum ={m} (100 Hidden Units")
+    plt.legend()
+    plt.show()
+    plt.clf()
+
+# Experiment 3
+nhidden = 100
+eta = .1
+momentum = .9
+train = [.25, .5]
+epochs = 50
+for t in train:
+    tr_c = t * len(xtrain)
+    ts_c = t * len(xtest)
+    test = perceptron.Perceptron(epochs, nhidden, eta, momentum)
+    # train() shuffles inputs and so the order is not always the same
+    conf_matrix, tr_acc, test_acc = test.train(xtrain[:tr_c], ytrain[:tr_c], xtest[:ts_c], ytest[:ts_c])
+    tr_acc = np.array(tr_acc)
+    tr_acc *= 100
+    test_acc = np.array(test_acc)
+    test_acc *= 100
+    print(conf_matrix)
+    plt.plot(tr_acc, label="Training Accuracy")
+    plt.plot(test_acc, label="Testing Accuracy")
+    plt.xlabel('Epochs')
+    plt.ylabel('Percent Correct')
+    plt.title(f"Testing vs Training Accuracy with {tr_c} Training Examples and {ts_c} Testing Examples (100 Hidden Units)")
+    plt.legend()
+    plt.show()
+    plt.clf()
--- perceptron.py
+++ perceptron.py
@@ -1,63 +1,92 @@
 # This is the perceptron code,
 import numpy as np
+from sklearn.utils import shuffle
 
 class Perceptron():
-    def __init__(self, nhidden, eta, momentum):
+    def __init__(self, epochs, nhidden, eta, momentum):
         self.nhidden = nhidden
         self.eta = eta
         self.momentum = momentum
-        self.hidden_units = np.zeroes(nhidden+1)
+        self.hidden_units = np.zeros((nhidden+1))
         #set bias
         self.hidden_units[-1] = 1
-        self.output = np.zeroes(10)
-
-    # Train on whole set.
-    def train(self, inputs, labels):
-        # Pick inputs 1 by 1
-        # input to hidden units
-        # not adding weight to hidden unit bias, only to array of hidden units
-        self.weights1 = np.random.rand((len(inputs[0]),self.nhidden)) - .5
+        self.output = np.zeros(10)
+        self.epochs = epochs
+        self.num_correct = 0
+        # input to hidden units. Inputs have 785 features.
+        # not adding weight to hidden unit bias
+        self.weights1 = np.random.rand(785, self.nhidden) - .5
+        self.delta_tm1_Wij = np.zeros((785, self.nhidden))
         # hidden units (plus bias) to output
-        self.weights2 = np.random.rand((self.nhidden+1, 10)) - .5
+        self.weights2 = np.random.rand(self.nhidden+1, 10) - .5
         # setting these to 0 for backprop later delta at time t - 1 for weights
         self.delta_tm1_Wkj = np.zeros((self.nhidden+1, 10))
-        self.delta_tm1_Wij = np.zeros((len(inputs[0], self.nhidden)))
 
-        print("training 1")
-        self.forwardProp(inputs[0])
-        self.checkCorrect(inputs[0],labels[0])
+    # this sigmoid function should allow me to calculate sigmoid activations for all hidden units
+    def sigmoid(self, array):
+        return 1 / (1 + np.exp(-array))
+
+    # Train on whole set.
+    def train(self, inputs, labels, test_inputs, test_labels):
+        # Pick inputs 1 by 1
+        train_acc = []
+        test_acc = []
+        for j in range(0, self.epochs):  # run training for 50 epochs
+            # Shuffle inputs at each epoch to avoid overfitting
+            inputs, labels = shuffle(inputs, labels, random_state=0)
+            # Resetting tm-1 deltas at each epoch
+            # self.delta_tm1_Wij = np.zeros((785, self.nhidden))
+            # self.delta_tm1_Wkj = np.zeros((self.nhidden + 1, 10))
+            if j % 10 == 0:
+                print("Epoch ", j)
+            for i in range(0, len(inputs)):
+                self.forwardProp(inputs[i])
+                label = self.checkCorrect(labels[i])
+                # label was copied in checkCorrect
+                self.backProp(inputs[i], label)
+            conf_matrix, tr_acc = self.test(inputs, labels)
+            train_acc.append(tr_acc)
+            conf_matrix, ts_acc = self.test(test_inputs, test_labels)
+            test_acc.append(ts_acc)
+        # return conf_matrix returns the final matrix of the test set.
+        return conf_matrix, train_acc, test_acc
+
+    def test(self, inputs, labels):
+        # Uses code from Test Correct to get all the testing code in one function so I can easily track accuracy
+        correct = 0
+        conf_matrix = np.zeros((10,10))
+        for i in range(0, len(inputs)):
+            self.forwardProp(inputs[i])
+            prediction = np.argmax(self.output)
+            # Making a copy of the labels with sigmoid activation scheme applied (no 1s or 0s)
+            if prediction == np.argmax(labels[i]):
+                correct += 1
+            conf_matrix[np.argmax(labels[i])][prediction] += 1
+        return conf_matrix, correct/len(inputs)
+
 
     def forwardProp(self, input):
-        # get activation of hidden layer, excluding bias node.
-        self.hidden_units[:self.nhidden] = np.dot(input, self.weights1)
-        self.output = np.dot(self.hidden_units, self.weights2)
+        # get activation of hidden layer, excluding bias node in hidden layer.
+        self.hidden_units[:self.nhidden] = self.sigmoid(np.dot(input, self.weights1))
+        self.output = self.sigmoid(np.dot(self.hidden_units, self.weights2))
 
-    def checkCorrect(self, input, label):
-        # If correct, return, move onto next input. If wrong, do backprop
+    def checkCorrect(self, label):
+        # Make prediction based on most activated output
         prediction = np.argmax(self.output)
-        # Set max prediction to .9
-        self.output[prediction] = .9
-        # convert outputs and labels to sigmoid activations
-        self.output = np.where(self.output == .9, .9, .1)
-        label = np.where(label == 1., .9, .1)
-        if prediction != np.argmax(label):
-            self.backProp(input, label)
-        # Return if correct
-        return
+        # adjust label for sigmoid function, making a copy instead of altering original
+        sigmoid_label = np.where(label == 1, .9, .1)
+        if prediction == np.argmax(sigmoid_label):
+            self.num_correct += 1
+        return sigmoid_label
 
     def backProp(self, input, label):
-        # We got the prediction wrong, need to backprop
-        # First, set output for sigmoid fn ( .9 already set for max prediction in checkCorrect() )
-        self.output = np.where(self.output == .9, .9, .1)
-
         # Update Weights for Hidden Units to Output:
         #   Delta(Wkj) = eta * sigk * Hj + momentum * (deltaT-1(Wkj))
         # Calculate error term sigk for output units, will add Hj as weights are updating.
-        sigmak = self.output(1 - self.output)(label-self.output) # should be 1x10
-        # Np.outer gives 10xn+1 matrix of Each Sigma multiplied by each Hj, resulting in a matrix
-        # of sigmak * hj for all combinations of sigmak and hj such that row 1 is W1*H1 ... W1Hn
+        sigmak = self.output * (1 - self.output) * (label - self.output) # should be 1x10
+        # transposing sigmak to get a 10x1, then dot producting it with hidden units(a 1x20
         # self.delta_tm1_wkj is a n+1x10 matrix, so I had to transpose the np.outer one to do elementwise add
-        delta_Wkj = self.eta * np.transpose(np.outer(sigmak, self.hidden_units)) + (self.momentum*self.delta_tm1_Wkj)
+        delta_Wkj = self.eta * np.transpose(np.dot(np.transpose([sigmak]), self.hidden_units.reshape(1, self.nhidden + 1))) + (self.momentum * self.delta_tm1_Wkj)
         # ^ this should currently be n+1x10
         self.weights2 = self.weights2 - delta_Wkj
         #  ^ should be n+1x10
@@ -67,10 +96,13 @@ class Perceptron():
         # Now Updating weights from input to hidden (Wij)
         # when updating input to hidden weights, make sure to stop at hidden[:nhidden] to avoid bias
         # self.hidden = 1x20 vector; self.weights2 = n+1x10, np.transpose(sigmak) = 10x1
-        # sigmaj should be 1x20 vector, each sum of all weights to hidden unit j
-        sigmaj = self.hidden_units (1-self.hidden_units) * np.dot(self.weights2, np.transpose(sigmak))
-        #                               V sigmaj now 20x1   V 1x785
-        delta_Wij = self.eta * np.transpose(np.dot(np.transpose(sigmaj)), input) + (self.momentum * self.delta_tm1_Wij)
-        #                       ^ Makes this array 785xn+1, just like delta_tm1_Wij
+        # sigmaj should be 1x20 vector, each sum of all weights to hidden unit j, not counting bias
+        sigmaj = self.hidden_units[:self.nhidden] * (1-self.hidden_units[:self.nhidden]) * \
+                 np.dot(self.weights2[:self.nhidden], np.transpose(sigmak))
+        #                                                V sigmaj now nx1   V 1x785
+        delta_Wij = self.eta * np.transpose(np.dot(np.transpose([sigmaj]), input.reshape(1, 785))) + (self.momentum * self.delta_tm1_Wij)
+        #                       ^ Makes this array 785xn, just like delta_tm1_Wij
+
         self.weights1 = self.weights1 - delta_Wij
+        self.delta_tm1_Wij = delta_Wij
         return
